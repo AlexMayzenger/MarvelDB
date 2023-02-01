2cebe91d135f03c5612150421e5ff14a4bda4e1f
AlexMayzenger <103361177+AlexMayzenger@users.noreply.github.com>
2023-02-01 18:53:21 +0700

    Рандомный персонаж

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -8,17 +8,29 @@ class MarvelService {
     getRecource = async (url) => {
         let res = await fetch(url);
         if (!res.ok) {
-            throw new Error(`Coild not fetch ${url}, status: ${res.status}`);
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
         return await res.json();
     }
 
-    getAllCharacters = ()=> {
-        return this.getRecource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async ()=> {
+        const res = await this.getRecource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter)
     } 
-    getCharacters = (id)=> {
-        return this.getRecource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id)=> {
+        const res = await this.getRecource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res);
     } 
+
+    _transformCharacter = (res) => {
+        return {
+            name: res.data.results[0].name,
+            description : res.data.results[0].description,
+            thumbnail: res.data.results[0].thumbnail.path + '.' + res.data.results[0].thumbnail.extension,
+            homepage: res.data.results[0].urls[0].url,
+            wiki: res.data.results[0].urls[1].url,
+}
+    }
 }
 
 
